ccf6746f8607ded6d01988f03bffce4d62ccc204
waseem_senjer <waseem_senjer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-03 15:15:11 +0000

    tagging 1.3.2
    git-svn-id: https://plugins.svn.wordpress.org/australian-post-woocommerce-extension/tags/1.3.2@1173691 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- australian-post.php
+++ australian-post.php
@@ -1,9 +1,9 @@
 <?php
 /* @wordpress-plugin
- * Plugin Name:       Australia Post WooCommerce Shipping Method
+ * Plugin Name:       Australia Post WooCommerce Extension
  * Plugin URI:        http://waseem-senjer.com/
  * Description:       WooCommerce Australian Post Shipping Method.
- * Version:           1.0.0
+ * Version:           1.3.2
  * Author:            Waseem Senjer
  * Author URI:        http://waseem-senjer.com
  * Text Domain:       australian-post
@@ -20,13 +20,13 @@ if(in_array('woocommerce/woocommerce.php', $active_plugins)){
 
 	add_filter('woocommerce_shipping_methods', 'add_australian_post_method');
 	function add_australian_post_method( $methods ){
-		$methods['australian-post'] = 'WC_Australian_Post_Shipping_Method';
+		$methods[] = 'WC_Australian_Post_Shipping_Method';
 		return $methods; 
 	}
 
 	add_action('woocommerce_shipping_init', 'init_australian_post');
 	function init_australian_post( ){
-		require_once 'class-australian-post.php';
+		require 'class-australian-post.php';
 	}
 
 }
